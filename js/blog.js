8c6c1bd7b1d46a145d31c2906d9791c3b2faa36b
daniel-certa-1228 <mfdancerta@gmail.com>
2017-08-05 23:00:36 -0500

    Views are woring alright so far

--- js/blog.js
+++ js/blog.js
@@ -1,2 +1,36 @@
 console.log( "blog.js" );
 
+{
+	let blogArray = [];
+	console.log( "blogArray", blogArray );
+
+	BlogSpace.passJSON = (blogObject) => {
+
+		for (let i = 0; i < blogObject.length; i++) {
+			let blogPost = blogObject[i]
+			blogArray.push(blogPost);
+		};
+	}
+
+	////////////////////////////////////////////////////
+
+	BlogSpace.outputToDOM = (blogArray) => {
+ 
+		let blogArticleDiv = document.getElementById("article-div");
+		blogArticleDiv.innerHTML = "";
+
+		for (let i = 0; i < blogArray.length; i++) {
+			// console.log( "test" );
+			let blogPost = '${blogArray[i].Title}'
+
+			console.log( "blogPost", blogPost );
+			
+			$(blogArticleDiv).prepend(blogPost)
+
+		};
+
+	}
+
+	BlogSpace.outputToDOM(blogArray);
+
+}
--- js/loader.js
+++ js/loader.js
@@ -3,3 +3,32 @@ console.log( "loader.jd" );
 {
 	var BlogSpace = {}
 }
+
+{
+	BlogSpace.loadBlogInfo = () => {
+
+		let blogObject = {};
+
+		let blogLoader = new XMLHttpRequest();
+		blogLoader.addEventListener("load", blogLoaderComplete);
+		blogLoader.addEventListener("error", blogLoaderFailed);
+		blogLoader.open("GET", "../json/blog.json")
+		blogLoader.send();
+
+		function blogLoaderComplete (event) {
+			console.log( "BlogLoader Complete" );
+			blogObject = JSON.parse(event.target.responseText); 
+			// console.log( "blogObject", blogObject );
+			BlogSpace.passJSON(blogObject)
+			// body...
+		}
+
+		function blogLoaderFailed (event) {
+			console.log( "BlogLoader Failed" );
+			// body...
+		}
+	}
+
+	BlogSpace.loadBlogInfo();
+
+}
